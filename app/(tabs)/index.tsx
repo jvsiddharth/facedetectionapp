0e0796455a7d8451e0a794f7855d77827eeabeca
jvsiddharth <104616265+jvsiddharth@users.noreply.github.com>
2024-06-01 13:02:04 +0530

    face detection1

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,45 +1,29 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import { Camera } from 'expo-camera';
+import { StyleSheet, View, ViewStyle } from 'react-native';
+import { PermissionStatus } from 'expo-modules-core';
+import { Camera, CameraView } from 'expo-camera';
 
 const App: React.FC = () => {
-  const [hasCameraPermission, setHasCameraPermission] = useState<boolean | null>(null);
-  const [facesDetected, setFacesDetected] = useState<any[]>([]);
+  const [hasCameraPermission, setHasCameraPermission] = useState<PermissionStatus | null>(null);
+
 
   useEffect(() => {
     (async () => {
       const { status } = await Camera.requestCameraPermissionsAsync();
-      setHasCameraPermission(status === 'granted');
+      setHasCameraPermission(status);
     })();
   }, []);
-
-  const handleFacesDetected = ({ faces }: { faces: any[] }) => {
-    setFacesDetected(faces);
-  };
 
   if (hasCameraPermission === null) {
     return <View />;
   }
-  if (!hasCameraPermission) {
-    return <Text>No access to camera</Text>;
+  if (hasCameraPermission !== PermissionStatus.GRANTED) {
+    return null; // You can return a different component or message here
   }
 
   return (
     <View style={styles.container}>
-      <Camera
-        style={styles.camera}
-        type={Camera.Constants.Type.front}
-        onFacesDetected={handleFacesDetected}
-        faceDetectorSettings={{
-          mode: Camera.Constants.FaceDetection.Mode.fast,
-          detectLandmarks: Camera.Constants.FaceDetection.Landmarks.none,
-          runClassifications: Camera.Constants.FaceDetection.Classifications.none,
-        }}
-      />
-      {/* Display face detection results */}
-      {facesDetected.map((face, index) => (
-        <View key={index} style={styles.faceRectangle} />
-      ))}
+      <CameraView style={styles.camera} facing='front' />
     </View>
   );
 };
@@ -47,20 +31,9 @@ const App: React.FC = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
   camera: {
     flex: 1,
-  },
-  faceRectangle: {
-    position: 'absolute',
-    borderWidth: 2,
-    borderColor: 'red',
-    top: face.bounds.origin.y,
-    left: face.bounds.origin.x,
-    width: face.bounds.size.width,
-    height: face.bounds.size.height,
   },
 });
 
